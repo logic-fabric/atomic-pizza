babfb8f4067aecaa03549014b1b57990ad7a7910
logic-fabric <lowic@logic-fabric.fr>
2025-02-24 06:53:46 +0100

    devtool (*): add Vite

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
+import { createElement, Fragment } from "react";
+import { createRoot } from "react-dom/client";
+
 function PizzaCard(props) {
-  return React.createElement("article", { class: "card" }, [
-    React.createElement("h2", null, props.name),
-    React.createElement("h3", null, "Ingrédients"),
-    React.createElement("p", null, props.ingredients),
-    React.createElement("p", { class: "price" }, props.price),
+  return createElement("article", { className: "card" }, [
+    createElement("h2", null, props.name),
+    createElement("h3", null, "Ingrédients"),
+    createElement("p", null, props.ingredients),
+    createElement("p", { className: "price" }, props.price),
   ]);
 }
 
 function App() {
-  return React.createElement(React.Fragment, null, [
-    React.createElement("h1", null, "Welcome to Atomic Pizza"),
-    React.createElement(
+  return createElement(Fragment, null, [
+    createElement("h1", null, "Welcome to Atomic Pizza"),
+    createElement(
       "div",
       {
         style: { display: "flex", flexWrap: "wrap", justifyContent: "center" },
       },
       [
-        React.createElement(PizzaCard, {
+        createElement(PizzaCard, {
           name: "Margarita",
           ingredients: "Tomate, mozzarella, jambon, basilic",
           price: "9,50 €",
         }),
 
-        React.createElement(PizzaCard, {
+        createElement(PizzaCard, {
           name: "Regina",
           ingredients: "Tomate, mozzarella, jambon, champignons, basilic",
           price: "10,50 €",
         }),
-        React.createElement(PizzaCard, {
+        createElement(PizzaCard, {
           name: "Hawaïenne",
           ingredients: "Tomate, mozzarella, jambon, ananas, basilic",
           price: "12,50 €",
@@ -38,6 +41,6 @@ function App() {
 }
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 
-root.render(React.createElement(App));
+root.render(createElement(App));
